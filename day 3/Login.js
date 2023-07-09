e9b1fec8a81335acef95a89b4a4b2fd404e7d122
VEL YOGESH <113898735+iamvelyogesh@users.noreply.github.com>
2023-07-09 22:45:19 +0530

    Add files via upload

---
+++ day 3/Login.js
@@ -0,0 +1,95 @@
+
+import './App.css';
+import React, { useEffect } from 'react';
+
+function Login() {
+
+    useEffect(() => {
+        const handleSignupClick = () => {
+          userForms.classList.remove('bounceRight');
+          userForms.classList.add('bounceLeft');
+        };
+    
+        const handleLoginClick = () => {
+          userForms.classList.remove('bounceLeft');
+          userForms.classList.add('bounceRight');
+        };
+    
+        const signupButton = document.getElementById('signup-button');
+        const loginButton = document.getElementById('login-button');
+        const userForms = document.getElementById('user_options-forms');
+    
+        signupButton.addEventListener('click', handleSignupClick);
+        loginButton.addEventListener('click', handleLoginClick);
+    
+        return () => {
+          // Clean up the event listeners when the component unmounts
+          signupButton.removeEventListener('click', handleSignupClick);
+          loginButton.removeEventListener('click', handleLoginClick);
+        };
+      }, []);
+
+  return (
+    <div >
+        
+<section class="user">
+  <div class="user_options-container">
+    <div class="user_options-text">
+      <div class="user_options-unregistered">
+        <h2 class="user_unregistered-title">Don't have an account?</h2>
+        <p class="user_unregistered-text">Feel free to join with us</p>
+        <button class="user_unregistered-signup" id="signup-button">Sign up</button>
+      </div>
+
+      <div class="user_options-registered">
+        <h2 class="user_registered-title">Have an account?</h2>
+        <p class="user_registered-text">Welcome Back My Friend</p>
+        <button class="user_registered-login" id="login-button">Login</button>
+      </div>
+    </div>
+    
+    <div class="user_options-forms" id="user_options-forms">
+      <div class="user_forms-login">
+        <h2 class="forms_title">Login</h2>
+        <form class="forms_form">
+          <fieldset class="forms_fieldset">
+            <div class="forms_field">
+              <input type="email" placeholder="Email" class="forms_field-input" required autofocus />
+            </div>
+            <div class="forms_field">
+              <input type="password" placeholder="Password" class="forms_field-input" required />
+            </div>
+          </fieldset>
+          <div class="forms_buttons">
+            <button type="button" class="forms_buttons-forgot">Forgot password?</button>
+            <input type="submit" value="Log In" class="forms_buttons-action"/>
+          </div>
+        </form>
+      </div>
+      <div class="user_forms-signup">
+        <h2 class="forms_title">Sign Up</h2>
+        <form class="forms_form">
+          <fieldset class="forms_fieldset">
+            <div class="forms_field">
+              <input type="text" placeholder="Full Name" class="forms_field-input" required />
+            </div>
+            <div class="forms_field">
+              <input type="email" placeholder="Email" class="forms_field-input" required />
+            </div>
+            <div class="forms_field">
+              <input type="password" placeholder="Password" class="forms_field-input" required />
+            </div>
+          </fieldset>
+          <div class="forms_buttons">
+            <input type="submit" value="Sign up" class="forms_buttons-action"/>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</section>
+    </div>
+  );
+}
+
+export default Login;
