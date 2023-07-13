f552119acd1a0aac7f6288253ae21122077b7e83
VEL YOGESH <113898735+iamvelyogesh@users.noreply.github.com>
2023-07-13 12:30:22 +0530

    Add files via upload

---
+++ day 6/src/App.js
@@ -0,0 +1,59 @@
+import React, {useState} from 'react'
+import Nav from './Nav'
+import Rout from './Rout';
+import {BrowserRouter} from 'react-router-dom';
+import Footer from './Footer';
+import Productdetail from './Productdetails';
+const App = () => {
+ 
+  const [cart, setCart] = useState([])
+  
+  const [close, setClose] = useState(false)
+  const [detail, setDetail] = useState([])
+  
+  const [product, setProduct] = useState(Productdetail)
+  const searchbtn = (product) => 
+  {
+    const change = Productdetail.filter((x) => 
+    {
+      return x.Cat === product
+    })
+    setProduct(change)
+  }
+  //product detail
+  const view = (product) => 
+  {
+    setDetail([{...product}])
+    setClose(true)
+  }
+
+  // add to cart
+  const addtocart = (product) => 
+  {
+    const exsit = cart.find((x) => 
+    {
+      return x.id === product.id
+    })
+    if(exsit)
+    {
+      alert("This Product is already added to cart")
+    }
+    else
+    { 
+      setCart([...cart, {...product, qty:1}])
+      alert("product is added to cart")
+    }
+  } 
+  console.log(cart)
+  return (
+    <>
+    <BrowserRouter>
+    <Nav searchbtn={searchbtn}/>
+    <Rout product={product} setProduct={setProduct} detail={detail} view={view} close={close} setClose={setClose} cart={cart} setCart={setCart} addtocart={addtocart}/>
+    <Footer />
+    </BrowserRouter>
+    </>
+  )
+}
+
+export default App
---
+++ day 6/src/Productdetails.js
@@ -0,0 +1,88 @@
+const Productdetail =
+[
+    {
+        id:1,
+        Title:"Elephant Figurine Hand Carved Expecting Mother, Soapstone Elephant Good Luck Symbol",
+        Cat: 'Ivory',
+        Price: '11,423',
+        Img: './img/tp1.jpg'
+    },
+    {
+        id:2,
+        Title:"Table Decor | Room Decor | Table Art | Artifacts | Housewarming Gifts",
+        Cat: 'Plastic',
+        Price: '849+',
+        Img: './img/tp2.jpg'
+    },
+    {
+        id:3,
+        Title:"Indian Artifacts - Beautifully Handmade Alter",
+        Cat: 'Decor',
+        Price: '36,423',
+        Img: './img/tp3.jpg'
+    },
+    {
+        id:4,
+        Title:"Handmade automatic Kung Fu tea set | Tea party tea set | Afternoon tea settp",
+        Cat: 'Setup',
+        Price: '7633',
+        Img: './img/tp4.jpg'
+    },
+    {
+        id:5,
+        Title:"Artifact 25",
+        Cat: 'Ivory',
+        Price: '6000',
+        Img: './img/tp5.jpg'
+    },
+    {
+        id:6,
+        Title:" Antique Artifact Handmade by Indian Artisans",
+        Cat: 'Plastic',
+        Price: '5629',
+        Img: './img/tp6.png'
+    },
+    {
+        id:7,
+        Title:"Handmade Replica of Ancient City of Hattusa Archaeological Artifact",
+        Cat: 'Decor',
+        Price: '2098',
+        Img: './img/tp7.jpg'
+    },
+    {
+        id:8,
+        Title:"Handmade Buddhist Healing Bowl | Sound Therapy | Meditation And Yoga",
+        Cat: 'Setup',
+        Price: '10102',
+        Img: './img/tp8.jpg'
+    },
+    {
+        id:9,
+        Title:"Prince",
+        Cat: 'Ivory',
+        Price: '10000',
+        Img: './img/tp9.jpg'
+    },
+    {
+        id:10,
+        Title:"Swarn Crafted by Indian Artisian",
+        Cat: 'Plastic',
+        Price: '5036',
+        Img: './img/tp10.jpg'
+    },
+    {
+        id:11,
+        Title:"Native Artifacts Southwest Metal Wall Sculpture by JasonW Studios",
+        Cat: 'Decor',
+        Price: '6000',
+        Img: './img/tp11.jpg'
+    },
+    {
+        id:12,
+        Title:"Artifact Of The Week Archives - The Museum Of Appalachia",
+        Cat: 'Setup',
+        Price: '5000',
+        Img: './img/tp12.jpg'
+    },
+]
+export default Productdetail;
